18bc7ce6b17e450d9a7a8403e8142a9b506d224a
Savio Abuga <savioabuga@gmail.com>
2017-09-09 10:42:34 +0300

    convert ListContacts from a stateless fn to stateful

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function ListContacts (props) {
-	return (
-		<ol className="contact-list">
-		{props.contacts.map((contact =>
-			<li className="contact-list-item" key={contact.id}>
-				<div className="contact-avatar" style={{
-					backgroundImage: `url(${contact.avatarURL})`
-				}} />
-				<div className="contact-details">
-					<p>{contact.name}</p>
-					<p>{contact.email}</p>
-				</div>
-				<button onClick={() => props.onDeleteContact(contact)} className="contact-remove">Remove</button>
-			</li>
-		))}
-	</ol>
-	)
-}
+class ListContacts extends Component {
+	static propTypes = {
+		onDeleteContact: PropTypes.func.isRequired,
+		contacts: PropTypes.array.isRequired
+	}
 
-ListContacts.propTypes = {
-	onDeleteContact: PropTypes.func.isRequired,
-	contacts: PropTypes.array.isRequired
+	render () {
+		return (
+			<ol className="contact-list">
+			{this.props.contacts.map((contact =>
+				<li className="contact-list-item" key={contact.id}>
+					<div className="contact-avatar" style={{
+						backgroundImage: `url(${contact.avatarURL})`
+					}} />
+					<div className="contact-details">
+						<p>{contact.name}</p>
+						<p>{contact.email}</p>
+					</div>
+					<button onClick={() => this.props.onDeleteContact(contact)} className="contact-remove">Remove</button>
+				</li>
+			))}
+		</ol>
+		)
+	}
 }
 
 export default ListContacts;
